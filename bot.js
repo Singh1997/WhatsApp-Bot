40b9f86e12c4eea7300bb8e14f16978c004f9b2b
Aman Singh <babuu9733@yahoo.com>
2018-04-29 04:22:00 +0530

    backend for sending a msg by specific name

---
+++ bot.js
@@ -0,0 +1,53 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const _ = require('lodash');
+
+const timeout = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://web.whatsapp.com', {waitUntil: ['domcontentloaded', 'networkidle0'], timeout: 0 });
+  await timeout(10000);
+  //page.on('console', msg => { console.log(msg) });
+  
+  global.sendMessage=async(index,msg)=>{
+    console.log('2');  
+    setInterval(()=>{
+    global.page.evaluate((i,m)=>{
+      Store.Chat.models[i].sendMessage(m);
+     },index,msg);
+    },6000);
+  }
+  await page.exposeFunction('passObject', (obj) => {
+  for(var x in obj){
+    console.log(obj[x].name);
+    if(obj[x].name=="Nitin Kiet"){
+      global.sendMessage(x,'?');
+     
+          }
+         
+  }
+  
+});
+
+  global.page = page;
+  setInterval(()=>{
+    console.log('1');  
+    global.page.evaluate(() => { 
+     window.passObject(Store.Chat.models) 
+   });
+  },7000);
+
+  await timeout(10000);
+  // await browser.close();
+
+
+
+  // await timeout(100);
+  // await global.sendMessage(page, "abhijeet", " If you are receiving this message, then, send functionility is done!!!")
+})();
